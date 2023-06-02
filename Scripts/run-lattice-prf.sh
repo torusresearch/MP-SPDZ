74a2ea81ad9a6381e211178897ba745597219f3c
Matthias Geihs <matthias@tor.us>
2023-06-02 08:57:48 +0200

    lattice_prf: set -lgp parameter

--- Scripts/run-lattice-prf.sh
+++ Scripts/run-lattice-prf.sh
@@ -3,5 +3,5 @@
 bit_length="${BIT_LENGTH:-256}"
 
 ./compile.py -F "$bit_length" lattice_prf &&
-./Scripts/mal-shamir-offline.sh lattice_prf &&
+./Scripts/mal-shamir-offline.sh -lgp 256 lattice_prf &&
 ./Scripts/mal-shamir.sh -F lattice_prf
