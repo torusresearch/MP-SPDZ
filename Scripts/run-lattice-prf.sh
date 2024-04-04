36473f6ddfeb93d0fcc2b5968021b6e3e4d92a63
Matthias Geihs <matthias@tor.us>
2024-04-04 16:11:44 +0200

    lattice prf: fix base field

--- Scripts/run-lattice-prf.sh
+++ Scripts/run-lattice-prf.sh
@@ -4,4 +4,4 @@ prog="${PROG:-lattice_prf}"
 
 ./compile.py -O $prog &&
 ./Scripts/mal-shamir-offline.sh -lgp 256 $prog &&
-./Scripts/mal-shamir.sh -F $prog
+./Scripts/mal-shamir.sh -lgp 256 -F $prog
