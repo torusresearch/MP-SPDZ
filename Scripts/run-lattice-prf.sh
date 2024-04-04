eedb162292ce44b150ca5585749bb77bdfb29efe
Matthias Geihs <matthias@tor.us>
2024-04-04 18:01:20 +0200

    use fixed prime

--- Scripts/run-lattice-prf.sh
+++ Scripts/run-lattice-prf.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 prog="${PROG:-lattice_prf}"
+prime="${PRIME:-57896044618658097711785492504343953926634992332820282019728792003956566065153}"
 
-./compile.py -O $prog &&
-./Scripts/mal-shamir-offline.sh -lgp 256 $prog &&
-./Scripts/mal-shamir.sh -lgp 256 -F $prog
+./compile.py -O -P $prime $prog &&
+./Scripts/mal-shamir-offline.sh -P $prime $prog &&
+./Scripts/mal-shamir.sh -P $prime -F $prog
