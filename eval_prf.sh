762a41aece0260180b5f29ddd14c2d070905c1bf
Matthias Geihs <matthias@tor.us>
2024-04-04 16:14:48 +0200

    eval_prf.sh: more parameters

--- eval_prf.sh
+++ eval_prf.sh
@@ -1,12 +1,16 @@
-PRIME=21888242871839275222246405745257275088548364400416034343698204186575808495617
+PRIME=21888242871839275222246405745257275088548364400416034343698204186575808495617 # 254 bit
+# PRIME=57896044618658097711785492504343953926634992332820282019728792003956566065153 # 256 bit
 SCRIPT_DIR=$(dirname "$0")
+T=1
+# MEASURE_COMMUNICATION="-- --batch-size 100"
 
 # Without preprocessing.
 for PROG in ciminion gmimc hades_mimc hydra mimc rescue
 do
 (
+    printf "\n$PROG\n"
     cd "$SCRIPT_DIR" \
-    && ./compile.py -O -P $PRIME $PROG 2 \
+    && ./compile.py -O -P $PRIME $PROG $T \
     && ./Scripts/mal-shamir-offline.sh $PROG \
     && ./Scripts/mal-shamir.sh -F $PROG
 ) 2>&1 | tee "log/eval_$PROG.log"
@@ -17,6 +21,6 @@ for PROG in ciminion gmimc hades_mimc hydra mimc rescue
 do
 (
     cd "$SCRIPT_DIR" \
-    && ./Scripts/compile-run.py -O -P $PRIME -E mal-shamir $PROG 2
+    && ./Scripts/compile-run.py -O -P $PRIME -E mal-shamir $PROG $T $MEASURE_COMMUNICATION
 ) 2>&1 | tee "log/eval_${PROG}_pre.log"
 done
